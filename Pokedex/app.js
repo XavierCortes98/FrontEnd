933b915810c01fbd9b0bdaade2dcd4044ab27978
Xavier <86173666+XavierCortes98@users.noreply.github.com>
2022-03-03 00:46:40 +0100

    expands info done

--- Pokedex/app.js
+++ Pokedex/app.js
@@ -12,40 +12,58 @@ function Pokemon(number) {
 }
 
 let pokemons = [];
+let lastTarget;
+
+
+
+function onClickDiv(e){   
+   console.log(e.currentTarget.children[2]);
+
+   if(lastTarget !== undefined && lastTarget!==e.currentTarget.children[2]){
+    lastTarget.classList.toggle("info-active");        
+   }
+   e.currentTarget.children[2].classList.toggle("info-active");
+   lastTarget = e.currentTarget.children[2];
+  
+    
+}
 
 async function basePokedex() {
     for (let i = 1; i < 10; i++) {
         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
         addPokemons(res.data);
     }
-
+    var classname = document.getElementsByClassName("data")
+    
+    for (let index = 0; index < classname.length; index++) {
+        classname[index].addEventListener('click', onClickDiv)
+    }
 }
 
-function click(e)
-{
+function click(e) {
     console.log(e.target);
     // e.target.children[2].style.maxHeight = e.target.children[2].scrollHeight + 'px';
-    
+
 }
 
 function addPokemons(data) {
     const li = document.createElement('li');
-    li.addEventListener("click", click);
+    //li.addEventListener("click", click);
 
 
     li.innerHTML = `
-    <div>
+    <div class="data">
         <div>
             <img class="pokemon" src="${data.sprites.front_default}" alt="">
         </div>
         <div>
             <p>${data.name}</p>
         </div>
-        <div>
-            <p class="data">${data.id}</p>
+        <div class="info">
+            <p>${data.id}</p>
         </div>  
-    </div>`;  
-   
+    </div>`;
+
     let pkmn = new Pokemon(data.name)
     pokemons.push(pkmn);
 
