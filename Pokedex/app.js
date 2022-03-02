f6f6aa5bad222e554fb820af76f0a9e32f9acb8e
Xavier <86173666+XavierCortes98@users.noreply.github.com>
2022-03-02 01:17:38 +0100

    style and basic call

--- Pokedex/app.js
+++ Pokedex/app.js
@@ -2,17 +2,61 @@ const searchForm = document.querySelector("#searchForm");
 const textInput = document.querySelector("#textInput");
 const searchButton = document.querySelector("#searchButton");
 
-function addPokemons(res)
-{
-    
-    const img = document.createElement('img');
-    img.src = res.data.sprites.front_default;
-    document.body.append(img);
+const list = document.querySelector("#list");
+
+function Pokemon(number) {
+    this.number = number;
+    // this.name = name;
+    // this.type = type;
+    // this.imgs = imgs;
 }
 
-searchForm.addEventListener("submit", async function (e) {
-    e.preventDefault();
-    let pokemonName = textInput.value;
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);   
-    addPokemons(res); 
-});
+let pokemons = [];
+
+async function basePokedex() {
+    for (let i = 1; i < 10; i++) {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        addPokemons(res.data);
+    }
+
+}
+
+function click(e)
+{
+    console.log(e.target);
+    // e.target.children[2].style.maxHeight = e.target.children[2].scrollHeight + 'px';
+    
+}
+
+function addPokemons(data) {
+    const li = document.createElement('li');
+    li.addEventListener("click", click);
+
+
+    li.innerHTML = `
+    <div>
+        <div>
+            <img class="pokemon" src="${data.sprites.front_default}" alt="">
+        </div>
+        <div>
+            <p>${data.name}</p>
+        </div>
+        <div>
+            <p class="data">${data.id}</p>
+        </div>  
+    </div>`;  
+   
+    let pkmn = new Pokemon(data.name)
+    pokemons.push(pkmn);
+
+    list.appendChild(li);
+}
+
+// searchForm.addEventListener("submit", async function (e) {
+//     e.preventDefault();
+//     let pokemonName = textInput.value;
+//     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);   
+//     addPokemons(res); 
+// });
+
+basePokedex();
